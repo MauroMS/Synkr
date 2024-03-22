e6f1a44809c2d4b331ddb869217deeb552068605
Mauro Schaparini <Mauro_Schaparini@GallagherRe.com>
2024-03-22 18:22:22 +0000

    test

---
+++ CloudSynkr.Models/Folder.cs
@@ -0,0 +1,11 @@
+﻿namespace CloudSynkr.Models;
+
+public class Folder
+{
+    public string Name { get; set; }
+    public string Path { get; set; }
+    public string Id { get; set; }
+    public string ParentId { get; set; }
+    public List<Folder> Children { get; set; } = new List<Folder>();
+    public List<File> Files { get; set; } = new List<File>();
+}
---
+++ CloudSynkr/CloudStorageService.cs
@@ -0,0 +1,108 @@
+﻿using CloudSynkr.Models;
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Drive.v3;
+using Google.Apis.Services;
+
+namespace CloudSynkr;
+
+public static class CloudStorageService
+{
+    private const string ParentIdTemp = "1FZPuYuAYRO3DkcN4H8Bfy8zh1884gjju";
+    
+     public static async Task<string> CreateFolder(string folderName, UserCredential credentials, string parentId, CancellationToken cancellationToken)
+    {
+        Console.WriteLine("CreateFolder");
+
+        using var driveService = new DriveService(new BaseClientService.Initializer()
+            { HttpClientInitializer = credentials, ApplicationName = "Synkr" });
+        
+        // File metadata
+        var fileMetadata = new Google.Apis.Drive.v3.Data.File()
+        {
+            Name = folderName,
+            MimeType = "application/vnd.google-apps.folder",
+            Parents = new List<string>
+            {
+                parentId
+            }
+        };
+        
+        // Create a new folder on drive.
+        var request = driveService.Files.Create(fileMetadata);
+        request.Fields = "id";
+        var file = await request.ExecuteAsync(cancellationToken);
+        // Prints the created folder id.
+        Console.WriteLine("Folder ID: " + file.Id);
+        Console.WriteLine("Folder Name: " + file.Name);
+        
+        return file.Id;
+    }
+    
+    public static async Task<List<Folder>> GetAllFoldersByParentId(UserCredential credentials, CancellationToken cancellationToken)
+    {
+        Console.WriteLine("GetAllFoldersByParentId");
+        // var credentials = await Login(cancellationToken);
+        var folders = new List<Folder>();
+
+        using var driveService = new DriveService(new BaseClientService.Initializer()
+            { HttpClientInitializer = credentials, ApplicationName = "Synkr" });
+        
+        var foldersRequest = driveService.Files.List();
+        foldersRequest.Fields = "files(id, name, mimeType, modifiedTime, parents)";
+        foldersRequest.Q = $"mimeType = '{FileType.Folder}' and trashed=false and '{ParentIdTemp}' in parents";
+        var listFoldersRequest = await foldersRequest.ExecuteAsync(cancellationToken);
+        folders.AddRange(listFoldersRequest.Files.Select(folder => new Folder() { Name = folder.Name, Id = folder.Id, ParentId = folder.Parents[0] }));
+
+        // Console.WriteLine($"Id: {folder.Id}");
+        // Console.WriteLine($"Name: {folder.Name}");
+        // Console.WriteLine($"Type: {folder.MimeType}");
+        // if (folder.Parents != null)
+        //     foreach (var parent in folder.Parents)
+        //     {
+        //         Console.WriteLine($"{parent}");
+        //     }
+        //
+        // Console.WriteLine($"-----------------------------------");
+        // Console.WriteLine($"-----------------------------------");
+
+        return folders;
+    }
+
+    public static async Task<List<string>> GetAllFilesFromFolder(UserCredential credentials, CancellationToken cancellationToken)
+    {
+        Console.WriteLine("GetAllFilesFromFolder");
+        using var driveService = new DriveService(new BaseClientService.Initializer()
+            { HttpClientInitializer = credentials, ApplicationName = "Synkr" });
+        
+        var filesRequest = driveService.Files.List();
+        filesRequest.Fields = "files(id, name, mimeType, modifiedTime)";
+        filesRequest.Q = $"mimeType = '{FileType.Folder}'";
+
+        var listFilesRequest = await filesRequest.ExecuteAsync(cancellationToken);
+
+        foreach (var file in listFilesRequest.Files)
+        {
+            Console.WriteLine($"{file.Name} - {file.MimeType}");
+        }
+
+        return null;
+    }
+
+    public static async Task<List<string>> GetAllItemsByQuery(UserCredential credentials, string query, CancellationToken cancellationToken)
+    {
+        Console.WriteLine("GetAllFoldersByParentId");
+        // var credentials = await Login(cancellationToken);
+        var items = new List<Folder>();
+
+        using var driveService = new DriveService(new BaseClientService.Initializer()
+            { HttpClientInitializer = credentials, ApplicationName = "Synkr" });
+        
+        var itemsRequest = driveService.Files.List();
+        itemsRequest.Fields = "files(id, name, mimeType, modifiedTime, parents)";
+        itemsRequest.Q = $"{query} and trashed=false";
+        var listFoldersRequest = await itemsRequest.ExecuteAsync(cancellationToken);
+        items.AddRange(listFoldersRequest.Files.Select(folder => new Folder() { Name = folder.Name, Id = folder.Id, ParentId = folder.Parents[0] }));
+
+        return items;
+    }
+}
